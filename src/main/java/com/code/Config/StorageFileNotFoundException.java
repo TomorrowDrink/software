50116c8d3d7e64d8503466a29e53fe26acde3973
顾杰荣1512190425 <562059776@qq.com>
2017-11-23 08:19:14 +0800

    文件上传下载

---
+++ src/main/java/com/code/Config/StorageFileNotFoundException.java
@@ -0,0 +1,14 @@
+package com.code.Config;
+
+import com.code.Config.StorageException;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/com/code/GraduationApplication.java
+++ src/main/java/com/code/GraduationApplication.java
@@ -1,13 +1,16 @@
 package com.code;
 
+import com.code.Config.StorageProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.code.Mapper")
+@EnableConfigurationProperties(StorageProperties.class)
 public class GraduationApplication extends SpringBootServletInitializer{
 
 
