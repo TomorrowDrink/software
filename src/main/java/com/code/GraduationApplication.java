2bbb69368113ac53ddd5bb7a60f33153780ff1de
顾杰荣1512190425 <562059776@qq.com>
2017-12-07 00:31:23 +0800

    学生端中成果物管理的所有页面基本完成；完善了不同成果（任务书，文献...论文等）文件的上传下载中的一些小问题。

--- src/main/java/com/code/GraduationApplication.java
+++ src/main/java/com/code/GraduationApplication.java
@@ -1,21 +1,28 @@
 package com.code;
 
-import com.code.Config.StorageProperties;
+//import com.code.Config.StorageProperties;
+import com.code.Service.StorageService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.code.Mapper")
-@EnableConfigurationProperties(StorageProperties.class)
+//@EnableConfigurationProperties(StorageProperties.class)
 public class GraduationApplication extends SpringBootServletInitializer{
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(GraduationApplication.class, args);
 	}
+
+//	@Bean
+//	CommandLineRunner init(StorageService storageService){
+//		return(args) -> {
+//			storageService.init();
+//		};
+//	}
 }
--- src/main/java/com/code/Service/lmpl/FileSystemStorageServicelmpl.java
+++ src/main/java/com/code/Service/lmpl/FileSystemStorageServicelmpl.java
@@ -3,31 +3,58 @@ package com.code.Service.lmpl;
 import com.code.Service.StorageService;
 import com.code.Config.StorageException;
 import com.code.Config.StorageFileNotFoundException;
-import com.code.Config.StorageProperties;
+//import com.code.Config.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
+@PreAuthorize("hasRole('ROLE_STUDENT')")
 @Service
 public class FileSystemStorageServicelmpl implements StorageService {
 
-    private final Path rootLocation;
+//    private final Path rootLocation;
+
+    private Path rootLocation;
+
+
+//    @Autowired
+//    public FileSystemStorageServicelmpl(StorageProperties properties) {
+//        this.rootLocation = Paths.get(properties.getLocation());
+//    }
 
     @Autowired
-    public FileSystemStorageServicelmpl(StorageProperties properties) {
-        this.rootLocation = Paths.get(properties.getLocation());
+    public FileSystemStorageServicelmpl( ) {
+        this.rootLocation = Paths.get("upload-dir/person");
+    }
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(rootLocation);
+        }
+        catch (IOException e) {
+            throw new StorageException("Could not initialize storage", e);
+        }
     }
 
     @Override
@@ -43,11 +70,100 @@ public class FileSystemStorageServicelmpl implements StorageService {
                         "Cannot store file with relative path outside current directory "
                                 + filename);
             }
+
+//重命名
+//            filename = filename + UUID.randomUUID();
+            Date date = new Date();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM");
+            String dir = simpleDateFormat.format(date);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+
+            if(filename.matches(".*assignment.*")){ filename ="a" + dateFormat.format(date) + "_" + file.getOriginalFilename(); }
+            if(filename.matches(".*review.*")){ filename ="b" + dateFormat.format(date) + "_" + file.getOriginalFilename();}
+            if(filename.matches(".*literature.*")){ filename ="c" + dateFormat.format(date) + "_" + file.getOriginalFilename();}
+            if(filename.matches(".*openingReport.*")){ filename ="d" + dateFormat.format(date) + "_" + file.getOriginalFilename();}
+            if(filename.matches(".*midterm.*")){ filename ="e" + dateFormat.format(date) + "_" + file.getOriginalFilename();}
+            if(filename.matches(".*process.*")){ filename ="f" + dateFormat.format(date) + "_" + file.getOriginalFilename();}
+            if(filename.matches(".*paper.*")){ filename ="g" + dateFormat.format(date) + "_" + file.getOriginalFilename();}
+
             Files.copy(file.getInputStream(), this.rootLocation.resolve(filename),
                     StandardCopyOption.REPLACE_EXISTING);
+
         }
         catch (IOException e) {
             throw new StorageException("Failed to store file " + filename, e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllAssignment() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*assignment.*"));
+//                    .filter(path -> !path.equals(this.rootLocation))
+//                    .map(path -> this.rootLocation.relativize(path)).sorted().limit(countA);
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllReview() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*review.*"));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllLiterature() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*literature.*"));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllOpeningReport() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*openingReport.*"));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllMidterm() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*midterm.*"));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllProcess() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*process.*"));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAllPaper() {
+        try {
+            return Files.find(this.rootLocation, 1,(path, basicFileAttributes) -> path.toFile().getName().matches(".*paper.*"));
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to read stored files", e);
         }
     }
 
@@ -61,12 +177,10 @@ public class FileSystemStorageServicelmpl implements StorageService {
         catch (IOException e) {
             throw new StorageException("Failed to read stored files", e);
         }
-
     }
 
     @Override
-    public Path load(String filename) {
-        return rootLocation.resolve(filename);
+    public Path load(String filename) { return rootLocation.resolve(filename);
     }
 
     @Override
@@ -74,9 +188,8 @@ public class FileSystemStorageServicelmpl implements StorageService {
         try {
             Path file = load(filename);
             Resource resource = new UrlResource(file.toUri());
-            if (resource.exists() || resource.isReadable()) {
+            if (resource.exists() || resource.isReadable())
                 return resource;
-            }
             else {
                 throw new StorageFileNotFoundException(
                         "Could not read file: " + filename);
@@ -91,15 +204,5 @@ public class FileSystemStorageServicelmpl implements StorageService {
     @Override
     public void deleteAll() {
         FileSystemUtils.deleteRecursively(rootLocation.toFile());
-    }
-
-    @Override
-    public void init() {
-        try {
-            Files.createDirectories(rootLocation);
-        }
-        catch (IOException e) {
-            throw new StorageException("Could not initialize storage", e);
-        }
     }
 }
